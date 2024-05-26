76a7976ec3447d500781305c68b0734ac2aeee4b
Michael Stewart <stewarmc@jmu.edu>
2024-05-26 04:47:26 +0000

    test the version updates

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,11 +2,13 @@
 git clone https://github.com/Lab-Lab-Lab/CPR-Music-Backend.git 
 git clone https://github.com/Lab-Lab-Lab/CPR-Music.git 
 cd CPR-Music-Backend 
+git checkout version-updates
 pip install -r requirements/local.txt --no-input 
 python manage.py migrate 
 # python manage.py runserver & 
 cd .. 
 cd CPR-Music 
+git checkout version-updates
 npm ci 
 # npm run dev & 
 cd ..
