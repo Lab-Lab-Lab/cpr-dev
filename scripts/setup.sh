fc3f07825bbe061367db8dafcea0f348fdfc1672
Michael Stewart <stewarmc@jmu.edu>
2024-05-26 04:37:42 +0000

    fix path

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/Lab-Lab-Lab/CPR-Music-Backend.git 
 git clone https://github.com/Lab-Lab-Lab/CPR-Music.git 
 cd CPR-Music-Backend 
-pip install -r CPR-Music/requirements/local.txt --no-input 
+pip install -r requirements/local.txt --no-input 
 python manage.py migrate 
 # python manage.py runserver & 
 cd .. 
