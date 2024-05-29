edcf3bfdbdac7c6b34778d88a124cae3be0ff3bd
Michael Stewart <stewarmc@jmu.edu>
2024-05-29 11:41:45 -0400

    setup env, and create default superuser

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,9 +2,12 @@
 git clone https://github.com/JMU-CIME/CPR-Music-Backend.git 
 git clone https://github.com/JMU-CIME/CPR-Music.git 
 cd CPR-Music-Backend 
+# probably will 
 git checkout version-updates
+cp .env.local .env 
 pip install -r requirements/local.txt --no-input 
 python manage.py migrate 
+python manage.py createsuperuser --no-input
 # python manage.py runserver & 
 cd .. 
 cd CPR-Music 
