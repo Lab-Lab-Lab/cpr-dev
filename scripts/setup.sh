c9b6213ae573e282fc707a3fd702b21dcf716a0c
Michael Stewart <stewarmc@jmu.edu>
2024-05-29 12:10:51 -0400

    replace .env.local with whatever is in the env vars. if this works for codespaces, does it mess up local devcontainers?

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,10 +8,13 @@ cp .env.local .env
 pip install -r requirements/local.txt --no-input 
 python manage.py migrate 
 python manage.py createsuperuser --no-input
-# python manage.py runserver & 
+# python manage.py runserver &  #put in postAttach instead
 cd .. 
 cd CPR-Music 
 git checkout version-updates
 npm ci 
-# npm run dev & 
+echo "NEXT_PUBLIC_BACKEND_HOST=\"https://$CODESPACE_NAME-8000.$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN\"" > .env.local
+# npm run dev & #put in postAttach instead
 cd ..
+
+
