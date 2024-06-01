75780950c8ebd07a746dd33556bff1a54eb8f7c5
Michael Stewart <stewarmc@jmu.edu>
2024-06-01 16:09:57 -0400

    update instructions

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-git clone https://github.com/JMU-CIME/CPR-Music-Backend.git 
-git clone https://github.com/JMU-CIME/CPR-Music.git 
+git clone https://github.com/Lab-Lab-Lab/CPR-Music-Backend.git 
+git clone https://github.com/Lab-Lab-Lab/CPR-Music.git 
 cd CPR-Music-Backend 
-# probably will 
-git checkout version-updates
 cp .env.local .env 
 echo "CORS_ALLOWED_ORIGIN_REGEXES=^http://localhost:3000$,^https://$CODESPACE_NAME-3000.$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN$"  >> .env
 pip install -r requirements/local.txt --no-input 
@@ -12,7 +10,6 @@ python manage.py createsuperuser --no-input
 # python manage.py runserver &  #put in postAttach instead
 cd .. 
 cd CPR-Music 
-git checkout version-updates
 npm ci 
 echo "NEXT_PUBLIC_BACKEND_HOST=\"https://$CODESPACE_NAME-8000.$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN\"
 NEXTAUTH_URL=\"https://$CODESPACE_NAME-3000.$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN\"
